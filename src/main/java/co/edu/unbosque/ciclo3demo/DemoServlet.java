76b05fbbde836916119c0ecaf50a7a49a6396eee
AlexYirsa <vyirsa@bancodebogota.com.co>
2021-09-16 20:35:02 -0500

    tercer commit 2021-16-09

---
+++ src/main/java/co/edu/unbosque/ciclo3demo/DemoServlet.java
@@ -0,0 +1,51 @@
+package co.edu.unbosque.ciclo3demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String nombre = request.getParameter("nombre");
+		String cedula = request.getParameter("cedula");
+		PrintWriter writer = response.getWriter();
+		if(nombre != null && cedula != null) {
+			writer.println(" Bienvenido "+nombre+" a mi JSP");
+		}
+		else
+			writer.println("Error: Nombre o cédula faltantes!");
+		writer.close();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/NewFile.jsp
+++ src/main/webapp/NewFile.jsp
@@ -4,10 +4,22 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>JSP de Ejemplo</title>
 </head>
 <body>
-	<h1>Prueba de mi primera pagina edicion</h1>
-
+	<h1>Mi Primer JSP</h1>
+	<form method="get" action="./DemoServlet">
+		<table>
+			<tr>
+				<td><label>Nombre:</label></td>
+				<td><input type="text" name="nombre"></td>
+				<td><label>Cedula:</label></td>
+				<td><input type="text" name="cedula"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="Enviar"></td>
+			</tr>	
+		</table>
+	</form>
 </body>
 </html>
