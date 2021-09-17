268628186036ac767c315272c1cd34635d37beff
AlexYirsa <vyirsa@bancodebogota.com.co>
2021-09-17 15:06:31 -0500

    nuevo comentario 2021-09-17

--- src/main/java/co/edu/unbosque/ciclo3demo/DemoServlet.java
+++ src/main/java/co/edu/unbosque/ciclo3demo/DemoServlet.java
@@ -39,7 +39,7 @@ public class DemoServlet extends HttpServlet {
 		else
 			writer.println("Error: Nombre o cédula faltantes!");
 		writer.close();
-		//prueba
+		//prueba de otro comentario mas
 		
 	}
 
