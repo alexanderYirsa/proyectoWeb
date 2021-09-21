9a30150b0acd6d43f203bf5a54487586312077db
AlexYirsa <vyirsa@bancodebogota.com.co>
2021-09-20 21:37:36 -0500

    Update Conexion.java

--- src/main/java/conexion/Conexion.java
+++ src/main/java/conexion/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexion {
 	
 	private static final String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost:8889/bd_estudiantes";
+	private static final String URL = "jdbc:mysql://localhost:8889/bd_tiendaWeb";
 	private static final String USUARIO = "root";
 	private static final String CLAVE = "root";
 	
