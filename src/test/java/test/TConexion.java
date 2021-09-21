e2e92e24a4a12d7e3f1ac259954fd682ee04aaaf
AlexYirsa <vyirsa@bancodebogota.com.co>
2021-09-20 20:55:39 -0500

    Conexion BD

---
+++ src/test/java/test/TConexion.java
@@ -0,0 +1,15 @@
+package test;
+
+import conexion.Conexion;
+
+public class TConexion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Conexion conectar = new Conexion();
+		conectar.conectar();
+
+	}
+
+}
